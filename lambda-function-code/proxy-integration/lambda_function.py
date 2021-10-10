479a585e05e9b4165035a9a217d32a4785f0fd0e
CloudNamasste <54523800+CloudNamaste@users.noreply.github.com>
2021-10-10 18:07:02 +1100

    Update lambda_function.py

--- lambda-function-code/proxy-integration/lambda_function.py
+++ lambda-function-code/proxy-integration/lambda_function.py
@@ -1,1 +1,76 @@
+import json
+import boto3
 
+
+def lambda_handler(event, context):
+    print(event)
+    operation = event["queryStringParameters"]["operation"]
+    body = json.loads( event["body"])
+    
+    dynamodb = boto3.resource('dynamodb')
+    
+    try:
+        table = dynamodb.Table('subscriber')
+        
+        if operation == 'scan':
+            print('List subscribers....')
+            response = table.scan()
+            
+        if operation == 'add':
+            print('Subscribing user....')
+            
+            response = table.put_item(
+               Item={
+                    'course': body["course"],
+                    'email': body["email"],
+                    'firstName': body["first_name"],
+                    'lastName': body["last_name"],
+                    'address': body["address"],
+                }
+            )
+        elif operation == 'get':
+            print('Get subscriber ....')
+            
+            response = table.get_item(
+                Key = {
+                    'course': event["queryStringParameters"]["course"],
+                    'email': event["queryStringParameters"]["email"]
+                })
+            
+        elif operation == 'update':
+            print('Updating subscriber adddress ....')
+
+            response = table.update_item(
+                Key={
+                    'course': body["course"],
+                    'email': body["email"]
+                },
+                UpdateExpression = 'set address = :val1',
+                ExpressionAttributeValues = {
+                    ':val1': address
+                },
+                ReturnValues = 'ALL_NEW'
+            )
+
+        elif operation == 'delete':
+            print('deleting subscriber ....')
+
+            response = table.delete_item(
+                Key={
+                    'course': event["queryStringParameters"]["course"],
+                    'email': event["queryStringParameters"]["email"]
+                }
+            )
+    except:
+        raise
+    
+    final_response = {
+        'statusCode' : 200,
+        'body': json.dumps(response),
+        'headers' : {
+            'Access-Control-Allow-Origin' :'*',
+            'Access-Control-Allow-Method' :'GET,PUT,POST,DELETE,OPTIONS'
+        }
+    }
+    print(final_response)
+    return final_response
