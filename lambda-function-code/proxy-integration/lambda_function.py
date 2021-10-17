e3a905d01def98912562e7126a5a95304f1e3726
CloudNamasste <54523800+CloudNamaste@users.noreply.github.com>
2021-10-17 14:57:48 +1100

    Update lambda_function.py

--- lambda-function-code/proxy-integration/lambda_function.py
+++ lambda-function-code/proxy-integration/lambda_function.py
@@ -10,7 +10,7 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     
     try:
-        table = dynamodb.Table('subscriber')
+        table = dynamodb.Table('subscribers')
         
         if operation == 'scan':
             print('List subscribers....')
