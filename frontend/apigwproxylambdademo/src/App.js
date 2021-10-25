b5b3cf5e3f6a2e5573e2e76bbd0f3403bcf1decb
CloudNamasste <54523800+CloudNamaste@users.noreply.github.com>
2021-10-25 20:21:25 +1100

    pushing code for proxy lambda demo

---
+++ frontend/apigwproxylambdademo/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomizedAccordions from './component/accordion.js'
+function App() {
+  return (
+    <div className="App">
+      <CustomizedAccordions/>
+    </div>
+  );
+}
+
+export default App;
